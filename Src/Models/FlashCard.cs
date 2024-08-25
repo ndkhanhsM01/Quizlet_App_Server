514f2ec9fa7ca7c323f27bb15f8a9579691134c4
ndkhanhs_m01 <142150794+ndkhanhsM01@users.noreply.github.com>
2024-08-26 00:32:42 +0700

    Add path image field for card model

--- Src/Models/FlashCard.cs
+++ Src/Models/FlashCard.cs
@@ -16,12 +16,14 @@ namespace Quizlet_App_Server.Models
         [BsonElement("time_created")] public long TimeCreated { get; set; } = TimeHelper.UnixTimeNow;
         //[BsonElement("is_public")] public bool IsPublic { get; set; } = false;
         [BsonElement("id_set_owner")] public string IdSetOwner { get; set; } = string.Empty;
+        [BsonElement("path_image")] public string PathImage { get; set; } = string.Empty;
         public FlashCard() { }
         public FlashCard(FlashCardDTO dto)
         {
             this.Term = dto.Term;
             this.Definition = dto.Definition;
             this.IdSetOwner = dto.IdSetOwner;
+            this.PathImage = dto.PathImage;
         }
     }
 
@@ -31,6 +33,7 @@ namespace Quizlet_App_Server.Models
         [BsonElement("term")] public string Term { get; set; } = string.Empty;
         [BsonElement("definition")] public string Definition { get; set; } = string.Empty;
         [BsonElement("id_set_owner")] public string IdSetOwner { get; set; } = string.Empty;
+        [BsonElement("path_image")] public string PathImage { get; set; } = string.Empty;
         //[BsonElement("is_public")] public bool IsPublic { get; set; } = false;
     }
 }
